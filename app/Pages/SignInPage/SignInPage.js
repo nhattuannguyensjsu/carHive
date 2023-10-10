f85de0d5c23faca9e03355461ca0e11dec869e23
sashaqty <eleashalaurico@yahoo.com>
2023-10-09 22:42:41 -0700

    first commit

--- app/Pages/SignInPage/SignInPage.js
+++ app/Pages/SignInPage/SignInPage.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet, useWindowDimensions, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, Button, Image, StyleSheet, useWindowDimensions, SafeAreaView} from 'react-native';
 import Logo from '../../../assets/images/logo.png';
 import CustomButton from '../../components/CustomButton/CustomButton';
+import SignUpButton from '../../components/CustomButton/SignUpButton';
 import { useNavigation } from '@react-navigation/native';
 import { FIREBASE_AUTH, FIREBASE_APP } from "../../../firebaseConfig";
 import { ActivityIndicator } from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, addDoc, collection } from "firebase/firestore";
+import Modal from "react-native-modal";
+
 
 const SignInPage = () => {
 
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
+
 
     const auth = FIREBASE_AUTH;
 
     const { height } = useWindowDimensions();
     const Navigation = useNavigation();
-
     const SignIn = async () => {
         setLoading(true);
         try {
@@ -31,14 +37,44 @@ const SignInPage = () => {
         }
     }
 
-    const onSignUpPage = () => {
-        Navigation.navigate('SignUpPage');
-    }
+    const SignUp = async () => {
+        if (email !== '' && password !== '') {
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              const user = userCredential.user;
+              // Use the user's email as the document ID
+              const userDocRef = doc(FIREBASE_DATABASE, "usersInfo", user.email);
+              // const signUpCollectionRef = collection(userDocRef, "UserInfo");
+              // Set user data in Firestore
+              setDoc(userDocRef, {
+                name: name,
+                email: email,
+                password: password,
+                // You may not want to store the password in Firestore for security reasons
+                // Instead, you can store other user-related data here
+              })
+                .then(() => {
+                  console.log('Data submitted');
+                  //Navigation.navigate("SignUpConfirmationPage");
+                })
+                .catch((error) => {
+                  console.error("Error writing document: ", error);
+                });
+            })
+            .catch((err) => {
+              console.error("Sign Up Error: ", err.message);
+            });
+        }
+      };
 
     const onForgotPasswordPressed = () => {
         Navigation.navigate('ForgotPasswordPage');
     }
+    const [isModalVisible, setIsModalVisible] = React.useState(false);
 
+    const handleModal = () => setIsModalVisible(() => !isModalVisible);
+  
+    
     return (
         <SafeAreaView style={styles.safe}>
             <View style={styles.root}>
@@ -82,15 +118,78 @@ const SignInPage = () => {
                         </>
                     )
                 }
+    
+                <SignUpButton text="Don't have an account? Click here to sign up" onPress={handleModal}/>
+                    <Modal
+                    isVisible={isModalVisible}>
 
-                <CustomButton
-                    text="Dont't have an account? Click here to sign up"
-                    onPress={onSignUpPage} />
+                    <View style={{ flex: 1 }}>
+                    <View style={styles.container}>
+                        <View style={styles.box1}>
+                        <View style={styles.root}>
+        
+        <Text style={styles.text_sub}> SIGN UP </Text>
 
+        <Text> Name </Text>
+        <TextInput
+          style={styles.input2}
+          required
+          type="text"
+          placeholder="John Smith"
+          onChangeText={(text) => setName(text)}
+          autoCapitalize="none"
+        >
+
+        </TextInput>
+
+        <Text> Email </Text>
+        <TextInput
+          style={styles.input2}
+          required
+          type="email"
+          placeholder="JohnSmith@gmail.com"
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none">
+        </TextInput>
+
+        <Text> Password </Text>
+        <TextInput
+          style={styles.input2}
+          required
+          type="password"
+          placeholder="Must be at least 6 characters"
+          onChangeText={(text) => setPassword(text)}
+          autoCapitalize="none"
+          secureTextEntry={true}>
+        </TextInput>
+
+        {loading ? <ActivityIndicator size="large" color="#0000ff" />
+          : (
+            <>
+             
+
+              <SignUpButton
+                text="Sign Up" onPress={() => SignUp()}/>
+
+            </>
+          )}
+
+        <SignUpButton
+          text="Already have an account? Click here to Login" onPress={handleModal} />
+  
+                    </View>
+                    </View>
+                    </View>
+                    </View>
+
+                  </Modal>
+                  
+                   
             </View>
         </SafeAreaView>
 
     );
+              
 };
 
 const styles = StyleSheet.create({
@@ -125,6 +224,30 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         marginVertical: 10,
     },
+    input2: {
+      backgroundColor: 'lightgrey',
+      width: '100%',
+      height: 40,
+      borderColor: 'white',
+      borderWidth: 1,
+      borderRadius: 20,
+      paddingHorizontal: 30,
+      marginVertical: 10,
+  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    box1:{
+    position: 'absolute',
+    borderRadius:20,
+    width: 320,
+    height: 600,
+    backgroundColor: 'white',
+   
+    justifyContent: "center",
+    }
 
 });
 
---
+++ app/components/CustomButton/SignUpButton.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+
+const CustomButton = ({ onPress, text, type }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.container, styles[`container_${type}`]]}
+    >
+      <Text style={[styles.text, styles[`text_${type}`]]}> {text} </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFD43C",
+    width: "90%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: 'center',
+    borderRadius: 20,
+    textAlign: "center"
+  },
+
+  container_primary: {
+    backgroundColor: "black",
+  },
+
+  text_tertiary: {
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+
+  text: {
+    color: "black",
+  },
+});
+
+export default CustomButton;
