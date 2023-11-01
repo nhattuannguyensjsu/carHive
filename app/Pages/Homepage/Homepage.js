137fe9eb685e65b082f139a14f8398bca96b425d
Nhat Tuan Nguyen <nhattuannguyen1@gmail.com>
2023-11-01 00:20:51 -0700

    search, filter, chat, updated posting

--- app/Pages/Homepage/Homepage.js
+++ app/Pages/Homepage/Homepage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Text, TextInput, Image, StyleSheet, useWindowDimensions } from 'react-native';
 import Logo from '../../../assets/images/logo.png';
 import Loc from '../../../assets/icons/location.png';
-import Swap from '../../../assets/icons/swap.png';
+import Sorting from '../../../assets/icons/swap.png';
 import Filter from '../../../assets/icons/filter.png';
 import Search from '../../../assets/icons/search.png';
 import CustomInput from '../../components/CustomInput/CustomInput';
@@ -20,6 +20,98 @@ const Homepage = () => {
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
     const [listingInfo, setListingInfo] = useState([]);
+
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredListingInfo, setFilteredListingInfo] = useState(null);
+
+    const [filterByPrice, setfilterByPrice] = useState(false);
+    const [sortByPrice, setSortByPrice] = useState(null);
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
+    const [sortOptionsVisible, setSortOptionsVisible] = useState(false);
+    const [locationInput, setLocationInput] = useState('');
+
+    const sortListingsByPrice = () => {
+        let sortedListings = [...(filteredListingInfo || listingInfo)];
+
+        if (sortByPrice === 'lowToHigh') {
+            sortedListings.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price));
+        } else if (sortByPrice === 'highToLow') {
+            sortedListings.sort((a, b) => parseFloat(b.Price) - parseFloat(a.Price));
+        }
+
+        setFilteredListingInfo(sortedListings);
+    };
+
+    const filterListingsByPriceRange = () => {
+        const filteredList = listingInfo.filter((item) => {
+            const price = parseFloat(item.Price);
+            const min = minPrice ? parseFloat(minPrice) : Number.MIN_SAFE_INTEGER;
+            const max = maxPrice ? parseFloat(maxPrice) : Number.MAX_SAFE_INTEGER;
+
+            return price >= min && price <= max;
+
+        });
+        setFilteredListingInfo(filteredList);
+    };
+    const handleFiltering = () => {
+        setfilterByPrice(!filterByPrice);
+        if (!filteredListingInfo) {
+            sortListingsByPrice();
+        }
+    };
+    const handleSorting = () => {
+        setSortOptionsVisible(!sortOptionsVisible);
+
+    };
+
+    const handleSortingOption = (option) => {
+        setSortByPrice(option);
+        sortListingsByPrice(option);
+
+    };
+    const searchListings = () => {
+        const keywords = searchInput.toLowerCase().split(' ');
+
+        const filteredList = listingInfo.filter((item) => {
+            const lowerCaseSearchInput = searchInput.toLowerCase();
+            const lowerCaseTitle = item.Title.toLowerCase();
+            const lowerCaseDescription = item.Description.toLowerCase();
+            const lowerCaseVIN = item.VIN.toLowerCase();
+            const lowerCaseLocation = item.Location.toLowerCase();
+
+            return keywords.some((keyword) => {
+                return lowerCaseTitle.includes(keyword) ||
+                    lowerCaseDescription.includes(keyword) ||
+                    lowerCaseVIN.includes(keyword) ||
+                    lowerCaseLocation.includes(lowerCaseSearchInput)
+
+            });
+        });
+        setFilteredListingInfo(filteredList);
+    };
+
+    const handleSearchInputChange = (text) => {
+        setSearchInput(text);
+    };
+
+    const handleSearchButtonClick = () => {
+        if (searchInput) {
+            searchListings(); // Filter listings based on the search input
+        } else {
+            setFilteredListingInfo(null); // Clear the filtered results
+        }
+    };
+
+    // useEffect(() => {
+    //     // If search input is empty, show all listings; otherwise, filter based on the search input
+    //     if (searchInput === '') {
+    //         setFilteredListingInfo(listingInfo);
+    //         getListingInfo();
+    //     } else {
+    //         searchListings();
+    //     }
+    // }, [searchInput, listingInfo]);
 
     const getListingInfo = async () => {
         try {
@@ -64,7 +156,7 @@ const Homepage = () => {
     //     } catch (error) {
     //         console.error('Error fetching user listings:', error);
     //     }
-    
+
 
 
     // Location.setGoogleApiKey("AIzaSyBSNJstpPFJMX-Z08gITore8Xwpl3Awg9Y");
@@ -84,9 +176,9 @@ const Homepage = () => {
         // };
         // getPermissions();
 
-         getListingInfo();
+        getListingInfo();
 
-      
+
     }, []);
 
 
@@ -106,12 +198,6 @@ const Homepage = () => {
     const { height } = useWindowDimensions();
     const Navigation = useNavigation();
 
-    const onSearchPressed = () => {
-        Navigation.navigate('SignInPage')
-    }
-
-
-
     return (
         <SafeAreaView style={styles.safe}>
             <View style={{ flex: 1 }}>
@@ -125,7 +211,7 @@ const Homepage = () => {
                     <Text style={styles.title}> CarHive </Text>
 
                 </View>
-{/* 
+                {/* 
                 <View style={{ flexDirection: 'row' }}>
 
                     <TouchableOpacity onPress={reverseGeocode}>
@@ -156,36 +242,119 @@ const Homepage = () => {
 
                     }}
                         placeholder='Search by Year, Model or Make'
-                        placeholderTextColor="black" />
-                    <Image source={require('../../../assets/icons/search.png')}
-                        resizeMode='contain'
-                        style={{
-                            width: 40, height: 40,
-                            marginTop: 15,
-                            marginLeft: 10,
-                        }} />
+                        placeholderTextColor="black"
+                        value={searchInput}
+                        onChangeText={handleSearchInputChange} />
+
+                    <TouchableOpacity
+                        onPress={handleSearchButtonClick}
+                    >
+                        <Image
+                            style={{
+                                width: 40,
+                                height: 40,
+                                marginTop: 15,
+                                marginLeft: 10,
+                            }}
+                            source={require('../../../assets/icons/search.png')} resizeMode='contain' />
+                    </TouchableOpacity>
                 </View>
 
-                <View style={{ flexDirection: 'row' }}>
-                    <Image source={Swap} style={[styles.icon_sub, { height: height * 0.3 }]}
+                {/* <View style={{ flexDirection: 'row' }}>
+                    <Image source={Sorting} style={[styles.icon_sub, { height: height * 0.3 }]}
                         resizeMode="contain"
                     />
                     <Image source={Filter} style={[styles.icon_sub1, { height: height * 0.3 }]}
                         resizeMode="contain"
                     />
+                </View> */}
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity
+                        onPress={handleSorting}
+                        style={{
+                            width: 20,
+                            height: 40,
+                            marginTop: 5,
+                            marginLeft: 10,
+                            marginRight: 15,
+                            marginBottom: 5,
+                        }}
+                    >
+                        <Image source={Sorting} style={[styles.icon_sub, { height: height * 0.3 }]}
+                            resizeMode="contain"
+                        />
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity
+                        onPress={handleFiltering}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            marginTop: 5,
+                        }}
+                    >
+                        <Image source={Filter} style={[styles.icon_sub, { height: height * 0.3 }]}
+                            resizeMode="contain"
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+
+                    {sortOptionsVisible && (
+                        <View style={styles.sortOptionsContainer}>
+                            <TouchableOpacity
+                                onPress={() => handleSortingOption('lowToHigh')}
+                                style={styles.sortOption}
+                            >
+                                <Text>Low to High</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => handleSortingOption('highToLow')}
+                                style={styles.sortOption}
+                            >
+                                <Text>High to Low</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                    {filterByPrice && (
+                        <View style={styles.priceRangeContainer}>
+                            <TextInput style={styles.filtering}
+                                placeholder="Min Price"
+                                keyboardType="numeric"
+                                value={minPrice}
+                                onChangeText={(text) => setMinPrice(text)}
+                            />
+                            <TextInput style={styles.filtering}
+                                placeholder="Max Price"
+                                keyboardType="numeric"
+                                value={maxPrice}
+                                onChangeText={(text) => setMaxPrice(text)}
+                            />
+
+                            <TouchableOpacity
+                                onPress={() => {
+                                    filterListingsByPriceRange(parseFloat(minPrice), parseFloat(maxPrice), locationInput);
+                                }}
+                            >
+                                <Text style={styles.button}> Search </Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
                 </View>
 
                 <FlatList
-                    data={listingInfo}
+                    data={filteredListingInfo || listingInfo}
                     keyExtractor={(item, index) => `${item.id}-${index}`}
-                    numColumns={2} // Set the number of columns to 2
+                    numColumns={2}
                     renderItem={({ item }) => (
                         <TouchableOpacity
                             style={styles.listingContainer}
                             onPress={() => {
-                                // Navigate to the "ListingPage" when an item is pressed
                                 Navigation.navigate('ListingPage', { listingData: item });
-                            }}>
+                            }}
+                        >
                             <View style={styles.listingPair}>
 
                                 <Image
@@ -198,7 +367,6 @@ const Homepage = () => {
 
                             </View>
                         </TouchableOpacity>
-
                     )}
                 />
 
@@ -227,13 +395,14 @@ const styles = StyleSheet.create({
         marginTop: 10,
         maxHeight: 30,
         maxWidth: 30,
-        marginLeft: "82%"
+        marginLeft: "82%",
     },
     icon_sub1: {
         marginTop: 10,
         maxHeight: 30,
         maxWidth: 30,
-        marginLeft: "1%"
+        marginLeft: "1%",
+
     },
     custom: {
         alignItems: 'flex-end',
@@ -302,6 +471,39 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: 'bold',
     },
+    filtering: {
+        backgroundColor: 'lightgrey',
+        width: "100%",
+        height: 30,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 15,
+        paddingHorizontal: 15,
+        textAlign: 'center',
+        marginLeft: 30,
+
+    },
+    sortOption: {
+        backgroundColor: 'lightgrey',
+        width: '80%',
+        height: 30,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 15,
+        paddingHorizontal: 15,
+        marginLeft: 30,
+        textAlign: 'center'
+    },
+
+    button: {
+        backgroundColor: "#FFD43C",
+        width: "100%",
+        padding: 8,
+        borderRadius: 20,
+        textAlign: 'center',
+        marginLeft: 30,
+
+    }
 
 });
 
--- app/Pages/PostPage/PostPage.js
+++ app/Pages/PostPage/PostPage.js
@@ -23,13 +23,17 @@ const PostPage = () => {
   const [location, setLocation] = useState('');
   const [zipcode, setZipcode] = useState('');
   const [VIN, setVIN] = useState('');
+  const [year, setYear] = useState('');
+  const [color, setColor] = useState('');
+  const [mileage, setMileage] = useState('');
+
   const [loading, setLoading] = useState(false);
   const [progress, setProgress] = useState(0);
   const [files, setFiles] = useState([]);
 
   const [pickedImage, setPickedImage] = useState("");
 
-  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false); // State for success modal
+  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
 
   async function pickImage() {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -41,19 +45,17 @@ const PostPage = () => {
 
     if (!result.canceled) {
       setPickedImage(result.assets[0].uri); // Set the picked image URL
-      // await uploadImage(result.assets[0].uri, "image");
     }
   }
 
   async function uploadImage(uri) {
     if (!pickedImage) {
-      // Handle error if no image is picked
       return;
     }
 
     const response = await fetch(uri);
     const blob = await response.blob();
-    const storageRef = ref(FIREBASE_STORAGE, "ChatImage/" + new Date().getTime());
+    const storageRef = ref(FIREBASE_STORAGE, "ListingPhoto/" + new Date().getTime());
 
     const uploadTask = uploadBytesResumable(storageRef, blob);
 
@@ -66,7 +68,6 @@ const PostPage = () => {
         setProgress(progress.toFixed());
       },
       (error) => {
-        // handle error
       },
       async () => {
         try {
@@ -93,6 +94,9 @@ const PostPage = () => {
         Location: location,
         Zipcode: zipcode,
         VIN: VIN,
+        Year: year,
+        Color: color,
+        Mileage: mileage,
         imageURL,
       });
       console.log("document saved correctly", userDocRef.id);
@@ -103,7 +107,7 @@ const PostPage = () => {
   }
 
   async function handleUpload() {
-    if (pickedImage && title && price && desc && Location && VIN) {
+    if (pickedImage && title && price && desc && Location && VIN && year && color && mileage) {
       await uploadImage(pickedImage, "image");
       setIsSuccessModalVisible(true);
       // Reset the input fields
@@ -113,6 +117,9 @@ const PostPage = () => {
       setVIN('');
       setLocation('');
       setZipcode('');
+      setMileage('');
+      setColor('');
+      setYear('');
       setPickedImage('');
 
     } else {
@@ -122,11 +129,8 @@ const PostPage = () => {
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(FIREBASE_DATABASE, "usersListing"), (snapshot) => {
-      // listen to changes in the collection in firestore
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
-          // if a new file is added, add it to the state
-          // console.log("New file", change.doc.data());
           setFiles((prevFiles) => [...prevFiles, change.doc.data()]);
         }
       });
@@ -151,7 +155,7 @@ const PostPage = () => {
           <Text style={styles.heading}>Post Item</Text>
 
           <View style={styles.body} >
-            {pickedImage ? ( // Render the picked image if it exists
+            {pickedImage ? (
               <Image
                 source={{ uri: pickedImage }}
                 style={{ width: 300, height: 100, marginTop: 20 }}
@@ -166,7 +170,7 @@ const PostPage = () => {
               </TouchableOpacity>
             )}
 
-            <Text style={styles.subheading}> Title </Text>
+            {/* <Text style={styles.subheading}> Title </Text> */}
             <TextInput style={styles.input}
               placeholder="Title"
               autoCapitalize="none"
@@ -175,19 +179,44 @@ const PostPage = () => {
             </TextInput>
 
 
-            <Text style={styles.subheading}> Price </Text>
-            <TextInput style={styles.input}
-              placeholder="Price"
-              keyboardType="numeric"
-              autoCapitalize="none"
-              value={price}
-              onChangeText={(text) => setPrice(text)}>
-            </TextInput>
+
+            {/* <Text style={styles.subheading}> Price </Text> */}
+            <View style={{ flexDirection: 'row' }}>
+              <TextInput style={styles.input2}
+                placeholder="Price"
+                keyboardType="numeric"
+                autoCapitalize="none"
+                value={price}
+                onChangeText={(text) => setPrice(text)}>
+              </TextInput>
+              <TextInput style={styles.input2}
+                placeholder="Year"
+                autoCapitalize="none"
+                value={year}
+                onChangeText={(text) => setYear(text)}>
+              </TextInput>
+            </View>
 
             <View style={{ flexDirection: 'row' }}>
+              <TextInput style={styles.input2}
+                placeholder="Color"
+                autoCapitalize="none"
+                value={color}
+                onChangeText={(text) => setColor(text)}>
+              </TextInput>
+              <TextInput style={styles.input2}
+                placeholder="Mileage"
+                keyboardType="numeric"
+                autoCapitalize="none"
+                value={mileage}
+                onChangeText={(text) => setMileage(text)}>
+              </TextInput>
+            </View>
+
+            {/* <View style={{ flexDirection: 'row' }}>
               <Text style={styles.subheading1}> Location </Text>
               <Text style={styles.subheading}> Zip Code </Text>
-            </View>
+            </View> */}
 
             <View style={{ flexDirection: 'row' }}>
               <TextInput style={styles.input2}
@@ -205,7 +234,7 @@ const PostPage = () => {
               </TextInput>
             </View>
 
-            <Text style={styles.subheading}> Description </Text>
+            {/* <Text style={styles.subheading}> Description </Text> */}
             <TextInput style={styles.input1}
               placeholder="Description"
               autoCapitalize="none"
@@ -214,7 +243,7 @@ const PostPage = () => {
               onChangeText={(text) => setDesc(text)}>
             </TextInput>
 
-            <Text style={styles.subheading} > Vehicle VIN </Text>
+            {/* <Text style={styles.subheading} > Vehicle VIN </Text> */}
             <TextInput style={styles.input}
               placeholder="VIN"
               autoCapitalize="none"
@@ -297,11 +326,12 @@ const styles = StyleSheet.create({
   input1: {
     backgroundColor: 'lightgrey',
     width: '90%',
-    height: 80,
+    height: 150,
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 20,
     paddingHorizontal: 20,
+    paddingVertical: 10,
     marginVertical: 10,
   },
   input2: {
